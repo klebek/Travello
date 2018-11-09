fec34fd1d03cc4ae1c8778cd810a442e382e86b1
klebek <s13928@pjwstk.edu.pl>
2018-11-09 13:09:47 +0100

    Still developing profiles

--- src/app/core/components/business/business.component.ts
+++ src/app/core/components/business/business.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-business',
@@ -7,7 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BusinessComponent implements OnInit {
 
-  constructor() { }
+  showNavigationArrows = false;
+  showNavigationIndicators = false;
+  // images = [1, 2, 3].map(() => `https://picsum.photos/900/450?random&t=${Math.random()}`);
+  images = [1, 2, 3].map(() => `http://graphlocks.pl/projekt_pjatk/business/${Math.floor((Math.random() * 3) + 1)}.jpg`);
+
+  constructor(config: NgbCarouselConfig) {
+    config.showNavigationArrows = true;
+    config.showNavigationIndicators = true;
+    config.interval = 10000;
+    config.wrap = true;
+    config.keyboard = false;
+    config.pauseOnHover = false;
+  }
 
   ngOnInit() {
   }
