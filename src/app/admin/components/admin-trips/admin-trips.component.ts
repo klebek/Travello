148cad3942beb8d1cd1ac7c59dc23b8391495133
klebek <s13928@pjwstk.edu.pl>
2018-12-19 09:19:16 +0100

    fixing filtering manage trips

--- src/app/admin/components/admin-trips/admin-trips.component.ts
+++ src/app/admin/components/admin-trips/admin-trips.component.ts
@@ -11,7 +11,7 @@ import { Trip } from 'app/travelling/model/trip';
   templateUrl: './admin-trips.component.html',
   styleUrls: ['./admin-trips.component.css']
 })
-export class AdminTripsComponent {
+export class AdminTripsComponent implements OnDestroy{
 
   blocked = false;
 
@@ -19,12 +19,33 @@ export class AdminTripsComponent {
 
   subscription: Subscription
 
-  trips;
+  trips: Trip[];
+  filteredTrips: Trip[];
   
   constructor(config: NgbRatingConfig, private tripService: TripService) {
     config.max = 5;
     config.readonly = true;
-    this.subscription = this.tripService.getAll().subscribe(t => this.trips = t);
+    this.subscription = this.tripService.getAll()
+      .subscribe((t: Trip[])=> this.filteredTrips = this.trips = t);
+  }
+
+  filterById(query1: number) {
+    let query1str = query1.toString();
+    this.filteredTrips = (query1str) ?
+      this.trips.filter(t => t.id.toString().toLowerCase().includes(query1str.toLowerCase())) : 
+      this.trips;
+  }
+
+  filterByTitle(query2: string) {
+    this.filteredTrips = (query2) ?
+      this.trips.filter(t => t.title.toLowerCase().includes(query2.toLowerCase())) : 
+      this.trips;
+  }
+
+  filterByTraveller(query3: string) {
+    this.filteredTrips = (query3) ?
+      this.trips.filter(t => t.title.toLowerCase().includes(query3.toLowerCase())) : 
+      this.trips;
   }
 
   block(id) {
@@ -35,6 +56,10 @@ export class AdminTripsComponent {
       this.tripService.changeStatus(id, this.trip, privateStatus);
       console.log(this.trip.status);
     });
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 
 }
